5f0836158eadd4933f32449b8aa5b2b4e0eed772
John Kosmicke <kozjob@yahoo.com>
2019-07-25 21:21:27 -0700

    update

--- model.js
+++ model.js
@@ -3,7 +3,27 @@
 
 model2048 = {
 
+  numSquares: 16,
+  values: [],
+
+
   init: function(  ) {
+    for( var i = 0; i < this.numSquares ; i++ ) {
+      this.values.push(0);
+    }
+    this.insertRandomTile();
+    this.insertRandomTile();
+  },
+
+
+  insertRandomTile: function(  ) {
+    var zeroes = [];
+    for( var i = 0; i < this.numSquares ; i++ ) {
+      if( this.values[i] === 0 ) zeroes.push(i);
+    }
+    var rand = Math.floor( Math.random() * zeroes.length );
+    var newValue = Math.random < 0.9 ? 2 : 4;
+    this.values[ zeroes[rand] ] = newValue;
   },
 };
 
--- view.js
+++ view.js
@@ -3,9 +3,26 @@
 view2048 = {
   model: model2048,
 
+  colors: [ "papayawhip", "darkkhaki", "coral", "orange", "aquamarine", "lime", "green", 
+    "dodgerblue", "mediumblue", "darkred", "purple", "brown", "black" ],
+  
   init: function(  ) {
-    this.$grid = $("#grid-2048");
-    this.$grid.append("<p>Tetris</p>");
+    this.update();
   },
+
+  update: function(  ) {
+    this.$grid = $("#grid-2048");
+    for( var i = 0; i < this.model.numSquares ; i++ ) {
+      var value = this.model.values[i] || "&nbsp";
+      var color = this.colors[0];
+      if( value > 0 ) color = this.colors[ Math.log2(value) ];
+      var $square = $("<div><div class='value'>" + value + "</div></div>");
+      $square.addClass("square");
+      $square.css("background-color", color);
+      this.$grid.append($square);
+    }
+  },
+
+
 };
 
